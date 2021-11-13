01e253385f4e5e34adab51a0158ea687767be9cf
ovum1411 <94230923+ovum1411@users.noreply.github.com>
2021-11-13 21:34:34 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,10 +2,10 @@ const nameGirl = '';
 const giftUrl = 'https://www.facebook.com/profile.php?id=100038400270844';
 const eventName = 'Chúc Mừng Sinh Nhật ';
 const titleCard = 'Tặng Minh Châu';
-const contentCard = 'Chúc bạn Châu sinh nhât tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với bạn trong hôm nay và cả những ngày sau';
+const contentCard = 'Chúc bạn Châu có trọn vẹn một ngày sinh nhât tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với bạn trong hôm nay và cả những ngày sau';
 
 // phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
 
-const giftImage = 'https://scontent.xx.fbcdn.net/v/t1.15752-9/s206x206/242705236_882650695956529_7454030627966191736_n.jpg?_nc_cat=104&ccb=1-5&_nc_sid=aee45a&_nc_ohc=bHqYZzGYVe8AX8P9sLR&_nc_oc=AQkV7wVgyO6qeTiVdZtttL5CLP0bfz3iZxK4quoWESv3MZ2vRLFhvU66-D5bt18_wd763DDlBT1rgGRKaZdHNYBY&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=006ce99cb6dfdb0fe7e6e125bba9e244&oe=61B5CAC2';
+const giftImage = 'https://scontent.fsgn5-9.fna.fbcdn.net/v/t1.15752-9/192155876_507914087304001_208238803725274880_n.jpg?_nc_cat=105&ccb=1-5&_nc_sid=ae9488&_nc_ohc=j9Fk2eUtrTwAX8O_OLx&_nc_ht=scontent.fsgn5-9.fna&oh=1e81383759a3fd09ae279df8a4e09665&oe=61B6EDCF';
 const base64 = '';
 const giftImageBase64 = "data:image/png;base64, " + base64;
