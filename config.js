9515be96b403665b3bf40cfa0bf26216f4e1e816
myweb <91318349+thaiinhhuynh@users.noreply.github.com>
2021-11-13 19:12:32 +0700

    Update config.js

--- config.js
+++ config.js
@@ -5,9 +5,7 @@ const titleCard = 'Tặng Minh Châu';
 const contentCard = 'Chúc bạn Châu sinh nhât tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với bạn trong hôm nay và cả những ngày sau';
 
 // phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
-// Cần hỗ trợ hãy liên hệ: 
-// Mr-Nam http://facebook.com/nam.nodemy
-// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+
 const giftImage = 'https://scontent.xx.fbcdn.net/v/t1.15752-9/s206x206/242705236_882650695956529_7454030627966191736_n.jpg?_nc_cat=104&ccb=1-5&_nc_sid=aee45a&_nc_ohc=bHqYZzGYVe8AX8P9sLR&_nc_oc=AQkV7wVgyO6qeTiVdZtttL5CLP0bfz3iZxK4quoWESv3MZ2vRLFhvU66-D5bt18_wd763DDlBT1rgGRKaZdHNYBY&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=006ce99cb6dfdb0fe7e6e125bba9e244&oe=61B5CAC2';
 const base64 = '';
 const giftImageBase64 = "data:image/png;base64, " + base64;
